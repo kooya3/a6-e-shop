c90df34a6e6224cfd69ad1ad70f8b55fb115518e
Elyees Tatua <elyeestatua@gmail.com>
2024-08-08 04:46:45 +0300

    style(HeaderComponent): remove MobileNav component

--- src/app/_components/Header/HeaderComponent/MobileNav/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MobileNav = () => {
-  return (
-    <div>MobileNav</div>
-  )
-}
-
-export default MobileNav
--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -15,10 +15,7 @@ export async function Header() {
   try {
     header = await fetchHeader()
   } catch (error) {
-    // When deploying this template on Payload Cloud, this page needs to build before the APIs are live
-    // So swallow the error here and simply render the header without nav items if one occurs
-    // in production you may want to redirect to a 404  page or at least log the error somewhere
-    // console.error(error)
+
   }
 
   return (
