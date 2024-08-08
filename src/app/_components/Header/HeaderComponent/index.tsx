91dbfa03c94d340d53eb89d6fa564348b7a416a8
Elyees Tatua <elyeestatua@gmail.com>
2024-08-08 03:41:28 +0300

    fix(Add): Payload logo to Header component

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+import Link from 'next/link'
 
 const HeaderComponent = ({ header }: { header: Header }) => {
   return (
-    <div>HeaderComponent</div>
+    <nav>
+        <Gutter>
+            <Link href="/">
+            <img src="/logo-black.svg" alt="logo" />
+            </Link>
+        </Gutter>
+    </nav>
   )
 }
 
