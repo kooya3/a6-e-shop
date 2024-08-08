855c587aac26e9b02dbe071a3d6eb1a38951027a
Elyees Tatua <elyeestatua@gmail.com>
2024-08-08 03:44:19 +0300

    fix(Remove): unused import and add Payload logo to Header component

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -3,9 +3,11 @@ import { Header } from '../../../../payload/payload-types'
 import { Gutter } from '../../Gutter'
 import Link from 'next/link'
 
+import classes from './index.module.scss'
+
 const HeaderComponent = ({ header }: { header: Header }) => {
   return (
-    <nav>
+    <nav className={[classes.header]}>
         <Gutter>
             <Link href="/">
             <img src="/logo-black.svg" alt="logo" />
