c2968842465ed433501a97fff5c1bd30f5f19b8d
Elyees Tatua <elyeestatua@gmail.com>
2024-08-08 19:12:11 +0300

    refactor: Update login form redirection logic

--- src/app/(pages)/login/LoginForm/index.tsx
+++ src/app/(pages)/login/LoginForm/index.tsx
@@ -36,7 +36,8 @@ const LoginForm: React.FC = () => {
       try {
         await login(data)
         if (redirect?.current) router.push(redirect.current as string)
-        else router.push('/account')
+        else router.push('/')
+      window.location.href="/"
       } catch (_) {
         setError('There was an error with the credentials provided. Please try again.')
       }
