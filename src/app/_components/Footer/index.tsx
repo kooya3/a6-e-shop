86b63e4fdaca63b099bcde1e02ac04054061ba5a
Elyees Tatua <elyeestatua@gmail.com>
2024-08-08 04:55:00 +0300

    style(Footer): remove error handling in Footer component

--- src/app/_components/Footer/index.tsx
+++ src/app/_components/Footer/index.tsx
@@ -15,10 +15,7 @@ export async function Footer() {
   try {
     footer = await fetchFooter()
   } catch (error) {
-    // When deploying this template on Payload Cloud, this page needs to build before the APIs are live
-    // So swallow the error here and simply render the footer without nav items if one occurs
-    // in production you may want to redirect to a 404  page or at least log the error somewhere
-    // console.error(error)
+
   }
 
   const navItems = footer?.navItems || []
--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link'
 
 import { Header } from '../../../payload/payload-types'
 import { fetchHeader } from '../../_api/fetchGlobals'
-import  HeaderComponent  from './HeaderComponent'
+import HeaderComponent from './HeaderComponent'
 
 export async function Header() {
   let header: Header | null = null
@@ -15,7 +15,7 @@ export async function Header() {
   try {
     header = await fetchHeader()
   } catch (error) {
-
+    console.log(error)
   }
 
   return (
