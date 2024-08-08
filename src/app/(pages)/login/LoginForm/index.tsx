dff2f34fe57c499abcfe3fdd898125672563438d
Elyees Tatua <elyeestatua@gmail.com>
2024-08-08 19:09:34 +0300

    refactor: Remove unnecessary paragraph in login form

--- src/app/(pages)/login/LoginForm/index.tsx
+++ src/app/(pages)/login/LoginForm/index.tsx
@@ -46,10 +46,6 @@ const LoginForm: React.FC = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={classes.form}>
-      <p>
-        {`This is where your customers will login to manage their account, review their order history, and more. To manage all users, `}
-
-      </p>
       <Message error={error} className={classes.message} />
       <Input
         name="email"
