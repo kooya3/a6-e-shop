d395d16a503c5f83eaa2f03b375ca7dfb9f06d2b
Elyees Tatua <elyeestatua@gmail.com>
2024-08-08 22:00:03 +0300

    refactor: Fix image path in login page

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -36,7 +36,7 @@ export default async function Login() {
 
           <div className={classes.formTitle}>
             <h3>Welcome</h3>
-            <Image src="/assests/icons/hand.png" alt="hand" width={30} height={30} />
+            <Image src="/assets/icons/hand.png" alt="hand" width={30} height={30} />
           </div>
 
           <p>Please login here</p>
