463c15bb1bbae58837db1e3004551feacb1e417c
Elyees Tatua <elyeestatua@gmail.com>
2024-08-08 03:46:11 +0300

    chore(Remove): unused import and add logo to Header component

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -8,7 +8,7 @@ import classes from './index.module.scss'
 const HeaderComponent = ({ header }: { header: Header }) => {
   return (
     <nav className={[classes.header]}>
-        <Gutter>
+        <Gutter className={classes.wrap}>
             <Link href="/">
             <img src="/logo-black.svg" alt="logo" />
             </Link>
