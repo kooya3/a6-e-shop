717860365cb9772c7b73274baf9ffccf3e2e4ef6
Elyees Tatua <elyeestatua@gmail.com>
2024-08-08 05:29:21 +0300

    refactor(Footer): Remove unused import and add inclusion icons to FooterComponent

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -1,15 +1,35 @@
+"use client";
+
 import React from 'react'
 
 import classes from './index.module.scss'
-import { noHeaderFooterUrls } from '../../../constants'
+import { inclusions, noHeaderFooterUrls } from '../../../constants'
 import { usePathname } from 'next/navigation'
+import { Gutter } from '../../Gutter'
+import Image from 'next/image';
 
 const FooterComponent = () => {
   const pathName = usePathname();
 
 
   return (
-    <footer className={noHeaderFooterUrls.includes(pathName) ? classes.hide : ''} />
+    <footer className={noHeaderFooterUrls.includes(pathName) ? classes.hide : ''}>
+      <Gutter>
+        <ul className={classes.inclusions}>
+          {inclusions.map((inclusion, index) => (
+            <li key={inclusion.title}>
+              <Image 
+              src={inclusion.icon}
+              alt={inclusion.title}
+              width={36}
+              height={36}
+              
+              />
+            </li>
+          )}
+        </ul>
+      </Gutter>
+    </footer>
   )
 }
 
