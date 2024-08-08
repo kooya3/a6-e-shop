74b12bd8b5fdb2c13771f510ea82bde2fe670f01
Elyees Tatua <elyeestatua@gmail.com>
2024-08-08 22:55:53 +0300

    refactor: Update recover password form message and layout

--- src/app/(pages)/recover-password/RecoverPasswordForm/index.tsx
+++ src/app/(pages)/recover-password/RecoverPasswordForm/index.tsx
@@ -50,14 +50,10 @@ export const RecoverPasswordForm: React.FC = () => {
     <Fragment>
       {!success && (
         <React.Fragment>
-          <h1>Recover Password</h1>
-          <div className={classes.formWrapper}>
-            <p>
-              {`Please enter your email below. You will receive an email message with instructions on
-              how to reset your password. To manage your all users, `}
-              <Link href="/admin/collections/users">login to the admin dashboard</Link>
-              {'.'}
-            </p>
+          <p>
+            Enter Your registered email address. We'll send you a code to reset your password.
+          </p>
+           
             <form onSubmit={handleSubmit(onSubmit)} className={classes.form}>
               <Message error={error} className={classes.message} />
               <Input
@@ -75,7 +71,6 @@ export const RecoverPasswordForm: React.FC = () => {
                 className={classes.submit}
               />
             </form>
-          </div>
         </React.Fragment>
       )}
       {success && (
