4adfe81b0216a19d0023799f36ac6f029530fb96
Elyees Tatua <elyeestatua@gmail.com>
2024-08-08 04:42:20 +0300

    style(HeaderComponent): optimize header visibility with lazy loading for images

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,34 +1,33 @@
-"use client";
+'use client'
 
 import React from 'react'
-import { Header } from '../../../../payload/payload-types'
-import { Gutter } from '../../Gutter'
-import Link from 'next/link'
-
-import classes from './index.module.scss'
 import Image from 'next/image'
-import { HeaderNav } from '../Nav'
-import MobileNav from './MobileNav'
-import { noHeaderFooterUrls } from '../../../constants'
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
-const HeaderComponent = ({ header }: { header: Header }) => {
-    const pathname  = usePathname()
+import { Header } from '../../../../payload/payload-types'
+import { noHeaderFooterUrls } from '../../../constants'
+import { Gutter } from '../../Gutter'
+import { HeaderNav } from '../Nav'
 
+import classes from './index.module.scss'
+
+const HeaderComponent = ({ header }: { header: Header }) => {
+  const pathname = usePathname()
 
   return (
-    <nav className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
-    .filter(Boolean)
-    .join(' ')}
+    <nav
+      className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+        .filter(Boolean)
+        .join(' ')}
     >
-        <Gutter className={classes.wrap}>
-            <Link href="/">
-            <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
-            </Link>
+      <Gutter className={classes.wrap}>
+        <Link href="/">
+          <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
+        </Link>
 
-            <HeaderNav header={header}/>
-            <MobileNav header={header} />
-        </Gutter>
+        <HeaderNav header={header} />
+      </Gutter>
     </nav>
   )
 }
