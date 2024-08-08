0821066f51530cba2a00d2dbb4181d2a36810cd9
Elyees Tatua <elyeestatua@gmail.com>
2024-08-08 23:24:00 +0300

    refactor(Add): Gutter component to page.tsx and CustomHero component to index.tsx
    Co-authored-by: Mwangi <mwangi8kevin@gmail.com>
    Co-authored-by: Suleiman Yunus <kayangasuleiman@gmail.com>
    Co-authored-by: Nelson Ombuya <nelson.ombuya@zohomail.com>
    Co-authored-by: Cynthia Muiga <cmuiga@dayliff.com>
    Co-authored-by: Steve Musebe <smusebe@dayliff.com>
    Co-authored-by: Anthony Waweru <awaweru@dayliff.com>
    Co-authored-by: Emelda Odhiambo <eodhiambo@dayliff.com>
    Co-authored-by: Abigail Visavilwa <avisavilwa@dayliff.com>
    Co-authored-by: E-vanson <evansonkamau233@gmail.com>
    Co-authored-by: KipkuruiYegon <kevinkipkurui1@gmail.com>

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -3,13 +3,14 @@ import { Metadata } from 'next'
 import { draftMode } from 'next/headers'
 import { notFound } from 'next/navigation'
 
-import { Page } from '../../../payload/payload-types'
+import { Category, Page } from '../../../payload/payload-types'
 import { staticHome } from '../../../payload/seed/home-static'
 import { fetchDoc } from '../../_api/fetchDoc'
 import { fetchDocs } from '../../_api/fetchDocs'
 import { Blocks } from '../../_components/Blocks'
 import { Hero } from '../../_components/Hero'
 import { generateMeta } from '../../_utilities/generateMeta'
+import { Gutter } from '../../_components/Gutter'
 
 // Payload Cloud caches all files through Cloudflare, so we don't need Next.js to cache them as well
 // This means that we can turn off Next.js data caching and instead rely solely on the Cloudflare CDN
@@ -23,6 +24,7 @@ export default async function Page({ params: { slug = 'home' } }) {
   const { isEnabled: isDraftMode } = draftMode()
 
   let page: Page | null = null
+  let categories: Category[] | null = null
 
   try {
     page = await fetchDoc<Page>({
@@ -30,6 +32,11 @@ export default async function Page({ params: { slug = 'home' } }) {
       slug,
       draft: isDraftMode,
     })
+
+    categories = await fetchDocs<Category>('categories');
+
+
+
   } catch (error) {
     // when deploying this template on Payload Cloud, this page needs to build before the APIs are live
     // so swallow the error here and simply render the page with fallback data where necessary
@@ -52,6 +59,21 @@ export default async function Page({ params: { slug = 'home' } }) {
 
   return (
     <React.Fragment>
+      {slug === 'home' ? (
+        <Gutter>
+          <Hero {...hero} />
+        </Gutter>
+      ) : (
+          <>
+            <Hero {...hero} />
+            <Blocks
+              blocks={layout}
+              disableTopPadding={!hero || hero?.type === 'none' || hero?.type === 'lowImpact'}
+            />
+          </>
+      )}
+
+
       <Hero {...hero} />
       <Blocks
         blocks={layout}
--- src/app/_heros/CustomHero/index.tsx
+++ src/app/_heros/CustomHero/index.tsx
@@ -11,5 +11,36 @@ import classes from './index.module.scss'
 export const CustomHero: React.FC<Page['hero']> = ({ richText, media, links }) => {
   return (
     
+     
+    <Gutter className={classes.hero}>
+        <h1>This is our custom hero</h1>
+      {/* <div className={classes.content}>
+        <RichText content={richText} />
+        {Array.isArray(links) && links.length > 0 && (
+          <ul className={classes.links}>
+            {links.map(({ link }, i) => {
+              return (
+                <li key={i}>
+                  <CMSLink {...link} />
+                </li>
+              )
+            })}
+          </ul>
+        )}
+      </div>
+      <div className={classes.media}>
+        {typeof media === 'object' && (
+          <Fragment>
+            <Media
+              resource={media}
+              // fill
+              imgClassName={classes.image}
+              priority
+            />
+            {media?.caption && <RichText content={media.caption} className={classes.caption} />}
+          </Fragment>
+        )}
+      </div> */}
+    </Gutter> 
   )
 }
