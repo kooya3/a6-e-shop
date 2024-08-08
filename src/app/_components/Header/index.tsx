0d09d7c91f584494a00936775b47f76b70701dd1
Elyees Tatua <elyeestatua@gmail.com>
2024-08-08 03:24:52 +0300

    chore(Add): Payload logo to header component

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -23,7 +23,23 @@ export async function Header() {
 
   return (
     <>
-      
+      <header className={classes.header}>
+        <Gutter className={classes.wrap}>
+          <Link href="/">
+            {/* Cannot use the `<picture>` element here with `srcSet`
+              This is because the theme is able to be overridden by the user
+              And so `@media (prefers-color-scheme: dark)` will not work
+              Instead, we just use CSS to invert the color via `filter: invert(1)` based on `[data-theme="dark"]`
+            */}
+            <img
+              className={classes.logo}
+              alt="Payload Logo"
+              src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/payload/src/admin/assets/images/payload-logo-light.svg"
+            />
+          </Link>
+          <HeaderNav header={header} />
+        
+      </header>
     </>
   )
 }
