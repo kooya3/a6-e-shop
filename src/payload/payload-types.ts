2c2d59087838d5868a1e823ceb2cc330bdb512e8
Elyees Tatua <elyeestatua@gmail.com>
2024-08-09 14:16:33 +0300

    refactor(Update): title field in Category interface and add media field to CollectionConfig
    Co-authored-by: Cynthia Muiga <bakhita650@gmail.com>

--- src/payload/payload-types.ts
+++ src/payload/payload-types.ts
@@ -178,7 +178,7 @@ export interface Media {
 }
 export interface Category {
   id: string;
-  title: string | null;
+  title: string;
   media?: string | Media | null;
   parent?: (string | null) | Category;
   breadcrumbs?:
