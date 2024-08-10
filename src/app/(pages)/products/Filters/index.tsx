c40909fbe9f2f0eaf92fd6683c9832c40e1ef7d1
Elyees Tatua <elyeestatua@gmail.com>
2024-08-11 01:35:08 +0300

    chore: Update Filters component in products page

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -1,18 +1,69 @@
+'use client'
+
 import React from 'react'
 
-import classes from './index.module.scss'
+import { Category } from '../../../../payload/payload-types'
+import { Checkbox } from '../../../_components/CheckBox'
+import { HR } from '../../../_components/HR'
+import { RadioButton } from '../../../_components/Radio'
 import { useFilter } from '../../../_providers/Filter'
 
-const Filters = () => {
-  const { categoryFilters, setCategoryFilters, sort, setSort } = useFilter();
+import classes from './index.module.scss'
+
+const Filters = ({ categories }: { categories: Category[] }) => {
+  const { categoryFilters, sort, setCategoryFilters, setSort } = useFilter()
+
+  const handleCategories = (categoryId: string) => {
+    if (categoryFilters.includes(categoryId)) {
+      const updatedCategories = categoryFilters.filter(id => id !== categoryId)
+
+      setCategoryFilters(updatedCategories)
+    } else {
+      setCategoryFilters([...categoryFilters, categoryId])
+    }
+  }
+
+  const handleSort = (value: string) => setSort(value)
 
   return (
-    <div className={classes.filers}><div>
+    <div className={classes.filters}>
+      <div>
         <h6 className={classes.title}>Product Categories</h6>
-        <div className={classes.Categories}>
+        <div className={classes.categories}>
+          {categories.map(category => {
+            const isSelected = categoryFilters.includes(category.id)
 
+            return (
+              <Checkbox
+                key={category.id}
+                label={category.title}
+                value={category.id}
+                isSelected={isSelected}
+                onClickHandler={handleCategories}
+              />
+            )
+          })}
         </div>
-    </div></div>
+        <HR className={classes.hr} />
+        <h6 className={classes.title}>Sort By</h6>
+        <div className={classes.categories}>
+          <RadioButton
+            label="Latest"
+            value="-createdAt"
+            isSelected={sort === '-createdAt'}
+            onRadioChange={handleSort}
+            groupName="sort"
+          />
+          <RadioButton
+            label="Oldest"
+            value="createdAt"
+            isSelected={sort === 'createdAt'}
+            onRadioChange={handleSort}
+            groupName="sort"
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
