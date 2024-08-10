5aaf601eaf06f8600d781e33ae7268edebd64e4e
Elyees Tatua <elyeestatua@gmail.com>
2024-08-10 20:01:39 +0300

    chore(Update): CategoryCard component to include category title in Link navigation to products page

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -12,7 +12,7 @@ const CategoryCard= ({ category }: CategoryCardProps ) => {
     const media = category.media as Media
 
   return (
-    <Link href="/products" className={classes.card} >
+    <Link href="/products" className={classes.card} style={{ backgroundImage: `url($media.url)`}}>
         <p className={classes.title}>{category.title}</p>
     </Link>
   )
--- src/app/_components/Categories/index.tsx
+++ src/app/_components/Categories/index.tsx
@@ -1,25 +1,23 @@
 import React from 'react'
-
-import classes from './index.module.scss'
 import Link from 'next/link'
+
+import { Category } from '../../../payload/payload-types'
 import CategoryCard from './CategoryCard'
 
+import classes from './index.module.scss'
 
 const Categories = ({ categories }: { categories: Category[] }) => {
   return (
     <section className={classes.container}>
       <div className={classes.titleWrapper}>
-        <h3>Shop By Categories</h3>
-        <Link href="/products">
-        Show More!
-        </Link>
+        <h3>Shop by Categories</h3>
+        <Link href="/products">Show All</Link>
+      </div>
 
-        <div className={classes.list}>
-          {categories.map((category) => (
-            <CategoryCard key={category.id} category={category} />
-          ))}
-
-        </div>
+      <div className={classes.list}>
+        {categories.map(category => {
+          return <CategoryCard key={category.id} category={category} />
+        })}
       </div>
     </section>
   )
