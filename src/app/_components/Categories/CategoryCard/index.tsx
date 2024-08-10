5a2aa6ad1e843baeb1116d499ae1119fdf544554
Elyees Tatua <elyeestatua@gmail.com>
2024-08-10 19:26:25 +0300

    chore(Update): CategoryCard component to include category title in Link navigation to products page

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -3,10 +3,12 @@ import React from 'react'
 import classes from './index.module.scss'
 import Link from 'next/link'
 
-const CategoryCard= () => {
+
+
+const CategoryCard= ({ category }: CategoryCardProps ) => {
   return (
     <Link href="/products" className={classes.card}>
-        
+        <p className={classes.title}>{Category.title}</p>
     </Link>
   )
 }
