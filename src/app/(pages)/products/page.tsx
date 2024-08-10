107c05db826faae15278e2fd0c0b9136c6549ce0
Elyees Tatua <elyeestatua@gmail.com>
2024-08-10 23:26:10 +0300

    feat(Add): Filters component to products page

--- src/app/(pages)/products/page.tsx
+++ src/app/(pages)/products/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import classes from './index.module.scss'
+import { Gutter } from '../../_components/Gutter'
 
 const products = () => {
   return (
-    <div className=''>products</div>
+    <div className={classes.container}>
+        <Gutter className={classes.products}>
+            <Filters />
+        </Gutter>
+    </div>
   )
 }
 
