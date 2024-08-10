d636aaaa3d63e03d14bf8cc8b96e9bc095fd347b
Elyees Tatua <elyeestatua@gmail.com>
2024-08-11 01:24:54 +0300

    chore: Update Filters component in products page

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
 import classes from './index.module.scss'
+import { useFilter } from '../../../_providers/Filter'
 
 const Filters = () => {
+  const { categoryFilters, setCategoryFilters, sort, setSort } = useFilter();
+
   return (
-    <div>Filters</div>
+    <div className={classes.filers}><div>
+        <h6 className={classes.title}>Product Categories</h6>
+        <div className={classes.Categories}>
+
+        </div>
+    </div></div>
   )
 }
 
