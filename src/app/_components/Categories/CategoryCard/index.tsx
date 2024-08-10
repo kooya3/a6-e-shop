5a1d3eb8199039587c0ff64177bfc446284aec7a
Elyees Tatua <elyeestatua@gmail.com>
2024-08-10 20:39:26 +0300

    chore(Update): CategoryCard component to include category title in Link navigation to products page

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -1,19 +1,28 @@
+'use client'
 import React from 'react'
+import Link from 'next/link'
+
+import { Category } from '../../../../payload/payload-types'
+import { useFilter } from '../../../_providers/Filter'
 
 import classes from './index.module.scss'
-import Link from 'next/link'
-import { Category, Media } from '../../../../payload/payload-types'
 
 type CategoryCardProps = {
-    category: Category
+  category: Category
 }
 
-const CategoryCard= ({ category }: CategoryCardProps ) => {
-    const media = category.media as Media
+const CategoryCard = ({ category }: CategoryCardProps) => {
+  const media = category.media as Media
+  const { setCategoryFilters } = useFilter()
 
   return (
-    <Link href="/products" className={classes.card} style={{ backgroundImage: `url($media.url)`}}>
-        <p className={classes.title}>{category.title}</p>
+    <Link
+      href="/products"
+      className={classes.card}
+      style={{ backgroundImage: `url(${media.url})` }}
+      onClick={() => setCategoryFilters([category.id])}
+    >
+      <p className={classes.title}>{category.title}</p>
     </Link>
   )
 }
