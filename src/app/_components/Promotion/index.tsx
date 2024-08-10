1f2f5f4d1123cf50a383bc2ada17596309f90547
Elyees Tatua <elyeestatua@gmail.com>
2024-08-10 22:51:44 +0300

    feat(Add): Deals of the Month section to Promotion component

--- src/app/_components/Promotion/index.tsx
+++ src/app/_components/Promotion/index.tsx
@@ -4,7 +4,13 @@ import classes from './index.module.scss'
 
 const Promotion = () => {
   return (
-    <section>Promotion</section>
+    <section className={classes.promotion}>
+        <div className={classes.textBox}>
+            <h3 className={classes.title}>
+                Deals of the Month
+            </h3>
+        </div>
+    </section>
   )
 }
 
