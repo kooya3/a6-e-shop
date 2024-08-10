0e1b24174993916e148478779856bc9a76ca0afa
Elyees Tatua <elyeestatua@gmail.com>
2024-08-11 01:48:58 +0300

    chore(Add): Radio component with styles and functionality

--- src/app/_components/Radio/index.tsx
+++ src/app/_components/Radio/index.tsx
@@ -4,4 +4,8 @@ import classes from './index.module.scss'
 
 interface RadioButtonProps {
   label: string
-  
+  value: string
+  isSelected: boolean
+  onRadioChange: (value: string) => void
+  groupName: string
+}
