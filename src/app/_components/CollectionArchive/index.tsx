ff982f435c19f487b18147e06ddbc0362937b510
Elyees Tatua <elyeestatua@gmail.com>
2024-08-11 17:07:48 +0300

    chore(Update): products page layout with grid display and gap spacing

--- src/app/_components/CollectionArchive/index.tsx
+++ src/app/_components/CollectionArchive/index.tsx
@@ -11,6 +11,7 @@ import { PageRange } from '../PageRange'
 import { Pagination } from '../Pagination'
 
 import classes from './index.module.scss'
+import { useFilter } from '../../_providers/Filter'
 
 type Result = {
   docs: (Product | string)[]
@@ -38,6 +39,8 @@ export type Props = {
 }
 
 export const CollectionArchive: React.FC<Props> = props => {
+  const { categoryFilters, sort } = useFilter();
+
   const {
     categories: catsFromProps,
     className,
@@ -116,10 +119,13 @@ export const CollectionArchive: React.FC<Props> = props => {
           page,
           sort,
           where: {
-            ...(categories
+            ...(categoryFilters && categoryFilters?.length > 0
               ? {
                   categories: {
-                    in: categories,
+                    in: 
+                    typeof catsFromProps === 'string'
+                    ? [categoryFilters]
+                    : categoryFilters.map((cat: string) => cat).join(''),
                   },
                 }
               : {}),
