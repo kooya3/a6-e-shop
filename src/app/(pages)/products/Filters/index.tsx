31594f66bcb4313edcf7c7ee903eb533ec3958ab
Elyees Tatua <elyeestatua@gmail.com>
2024-08-11 18:11:14 +0300

    chore(Refactor): Filters component in products page with category checkboxes and sort by functionality
    Co-authored-by: Cynthia Muiga <cmuiga@dayliff.com>
    Co-authored-by: Suleiman Yunus <kayangasuleiman@gmail.com>
    Co-authored-by: Mwangi <mwangi8kevin@gmail.com>
    Co-authored-by: E-vanson <evansonkamu233@gmail.com>
    Co-authored-by: KipkuruiYegon <kevinkipkurui1@gmail.com>
    Co-authored-by: Nelson Ombuya <nelson.ombuya@zohomail.com>

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -15,7 +15,7 @@ const Filters = ({ categories }: { categories: Category[] }) => {
   const {categoryFilters, sort, setCategoryFilters, setSort} = useFilter()
 
   const handleCategories = (categoryId: string) => {}
-  const handleSort = () =>  {}
+  const handleSort = (value: string) => setSort(value)
 
   return (
     <div className={classes.filters}>
