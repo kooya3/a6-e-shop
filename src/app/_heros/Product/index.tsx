bf831cbf5b0c9fa3a2f1085a8b7d6bd794016308
Elyees Tatua <elyeestatua@gmail.com>
2024-08-12 00:05:05 +0300

    Refactor(Product): Remove unnecessary code and improve link text in ProductHero component

--- src/app/_heros/Product/index.tsx
+++ src/app/_heros/Product/index.tsx
@@ -23,50 +23,38 @@ export const ProductHero: React.FC<{
   } = product
 
   return (
-      <Gutter className={classes.productHero}>
-          <div className={classes.categories}>
-            {categories?.map((category, index) => {
-              if (typeof category === 'object' && category !== null) {
-                const { title: categoryTitle } = category
+    <Gutter className={classes.productHero}>
+      <div className={classes.categories}>
+        {categories?.map((category, index) => {
+          if (typeof category === 'object' && category !== null) {
+            const { title: categoryTitle } = category
 
-                const titleToUse = categoryTitle || 'Untitled category'
+            const titleToUse = categoryTitle || 'Untitled category'
 
-                const isLast = index === categories.length - 1
+            const isLast = index === categories.length - 1
 
-                return (
-                  <Fragment key={index}>
-                    {titleToUse}
-                    {!isLast && <Fragment>, &nbsp;</Fragment>}
-                  </Fragment>
-                )
-              }
+            return (
+              <Fragment key={index}>
+                {titleToUse}
+                {!isLast && <Fragment>, &nbsp;</Fragment>}
+              </Fragment>
+            )
+          }
 
-              return null
-            })}
-          </div>
-          <h1 className={classes.title}>{title}</h1>
-          <div>
-            <p className={classes.description}>
-              {`${description ? `${description} ` : ''}To edit this product, `}
-              <Link href={`${process.env.NEXT_PUBLIC_SERVER_URL}/admin/collections/products/${id}`}>
-                navigate to the admin dashboard
-              </Link>
-              {'.'}
-            </p>
-          </div>
-          <Price product={product} button={false} />
-
-        <div className={classes.media}>
-          <div className={classes.mediaWrapper}>
-            {!metaImage && <div className={classes.placeholder}>No image</div>}
-            {metaImage && typeof metaImage !== 'string' && (
-              <Media imgClassName={classes.image} resource={metaImage} fill />
-            )}
-          </div>
-          {metaImage && typeof metaImage !== 'string' && metaImage?.caption && (
-            <RichText content={metaImage.caption} className={classes.caption} />
-          )}
-        </div>
+          return null
+        })}
+      </div>
+      <h1 className={classes.title}>{title}</h1>
+      <div>
+        <p className={classes.description}>
+          {`${description ? `${description} ` : ''}To edit this product, `}
+          <Link href={`${process.env.NEXT_PUBLIC_SERVER_URL}/admin/collections/products/${id}`}>
+            navigate to the admin dashboard
+          </Link>
+          {'.'}
+        </p>
+      </div>
+      <Price product={product} button={false} />
       </Gutter>
   )
 }
