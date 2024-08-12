83cde3dffa9745f1d8285841c6064aba45571ea4
Elyees Tatua <elyeestatua@gmail.com>
2024-08-12 16:00:55 +0300

    refactor(CheckoutPage): Include lazy loading for images and improve code formatting

--- src/app/(pages)/checkout/page.tsx
+++ src/app/(pages)/checkout/page.tsx
@@ -85,9 +85,7 @@ export default async function Checkout() {
                                             },
                                           ],
                                         },
-                                        {
-                                          text: ' like ',
-                                        },
+                                        
                                         
                                        
                                       ],
