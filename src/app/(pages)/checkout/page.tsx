9b08b075ea7cd5ba5848170576fd57264f4b9b79
Elyees Tatua <elyeestatua@gmail.com>
2024-08-12 18:26:43 +0300

    refactor(CheckoutPage): Remove unused code for test credit card link and include lazy loading for images

--- src/app/(pages)/checkout/page.tsx
+++ src/app/(pages)/checkout/page.tsx
@@ -52,7 +52,7 @@ export default async function Checkout() {
                                             >
                                               {'README'}
                                             </a>
-                                            {' for more details.'}
+                                        
                                           </Fragment>
                                         }
                                       />
