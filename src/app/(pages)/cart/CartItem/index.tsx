0ede6fc5873f9c2789d82ab10828b6117d2bfc01
Elyees Tatua <elyeestatua@gmail.com>
2024-08-12 03:20:41 +0300

    refactor(CartItem): Update CartItem component to include title

--- src/app/(pages)/cart/CartItem/index.tsx
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -5,7 +5,9 @@ import classes from './index.module.scss'
 const CartItem = () => {
   return (
     <li className={classes.item}>
-
+        <h6>
+            TITLE
+        </h6>
     </li>
   )
 }
