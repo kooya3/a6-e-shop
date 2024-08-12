bd5f6db93562c9f0d1e5a14aa815bb2fafcc8779
Elyees Tatua <elyeestatua@gmail.com>
2024-08-12 03:28:18 +0300

    refactor(CartItem): Update CartItem component to include lazy loading for images

--- src/app/(pages)/cart/CartItem/index.tsx
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -34,7 +34,17 @@ const CartItem = ({ product, title, metaImage, qty, addItemToCart }) => {
   }
 
   return (
-    
+    <li className={classes.item} key={title}>
+      <Link href={`/products/${product.slug}`} className={classes.mediaWrapper}>
+        {!metaImage && <span>No image</span>}
+        {metaImage && typeof metaImage !== 'string' && (
+          <Media className={classes.media} imgClassName={classes.image} resource={metaImage} fill />
+        )}
+      </Link>
+
+     
+ 
+    </li>
   )
 }
 
