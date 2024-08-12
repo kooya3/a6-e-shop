8629211ba2809701b19b480b0ba9979aaf1879dc
Elyees Tatua <elyeestatua@gmail.com>
2024-08-12 21:38:03 +0300

    refactor(CheckoutPage): Include lazy loading for images and remove unused code

--- src/app/(pages)/checkout/page.tsx
+++ src/app/(pages)/checkout/page.tsx
@@ -30,8 +30,8 @@ export default async function Checkout() {
 
   return (
     <div>
-      <Gutter>
-        <CheckoutPage />
+      <Gutter className={classes.checkoutPage}>
+        <CheckoutPage settings={settings} />
       </Gutter>
     </div>
   )
