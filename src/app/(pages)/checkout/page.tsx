4fce30023216567337eadf661835c22302e3a85d
Elyees Tatua <elyeestatua@gmail.com>
2024-08-12 18:30:51 +0300

    refactor(CheckoutPage): Remove unused code for test credit card link and include lazy loading for images

--- src/app/(pages)/checkout/page.tsx
+++ src/app/(pages)/checkout/page.tsx
@@ -45,11 +45,7 @@ export default async function Checkout() {
                                               {'obtain your Stripe API Keys'}
                                             </a>
                                             {' then set them as environment variables. See the '}
-                                            <a
-                                              
-                                            >
-                                            </a>
-                                        
+                                           
                                           </Fragment>
                                         }
                                       />
