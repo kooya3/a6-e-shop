7aaf8cc98177104c3bdc29d11716ce4233c816eb
Elyees Tatua <elyeestatua@gmail.com>
2024-08-12 03:25:25 +0300

    refactor(CartItem): Update CartItem component styling

--- src/app/(pages)/cart/CartItem/index.tsx
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -8,7 +8,13 @@ import { Media } from '../../../_components/Media'
 import { Price } from '../../../_components/Price'
 import { RemoveFromCartButton } from '../../../_components/RemoveFromCartButton'
 
+import classes from './index.module.scss'
 
+const CartItem = ({ product, title, metaImage, qty, addItemToCart }) => {
+  const [quantity, setQuantity] = useState(qty)
+
+ 
+    </li>
   )
 }
 
