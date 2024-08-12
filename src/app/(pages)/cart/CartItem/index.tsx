ce5465073ab93b1a69b1fe76a87e845341d0112d
Elyees Tatua <elyeestatua@gmail.com>
2024-08-12 03:29:07 +0300

    refactor(CartItem): Update CartItem component to include lazy loading for images, quantity input, and subtotal wrapper

--- src/app/(pages)/cart/CartItem/index.tsx
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -42,7 +42,46 @@ const CartItem = ({ product, title, metaImage, qty, addItemToCart }) => {
         )}
       </Link>
 
-     
+      <div className={classes.itemDetails}>
+        <div className={classes.titleWrapper}>
+          <h6>{title}</h6>
+          <Price product={product} button={false} />
+        </div>
+
+        <div className={classes.quantity}>
+          <div className={classes.quantityBtn} onClick={decrementQty}>
+            <Image
+              src="/assets/icons/minus.svg"
+              alt="minus"
+              width={24}
+              height={24}
+              className={classes.qtnBt}
+            />
+          </div>
+
+          <input
+            type="text"
+            className={classes.quantityInput}
+            value={quantity}
+            onChange={enterQty}
+          />
+
+          <div className={classes.quantityBtn} onClick={incrementQty}>
+            <Image
+              src="/assets/icons/plus.svg"
+              alt="plus"
+              width={24}
+              height={24}
+              className={classes.qtnBt}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className={classes.subtotalWrapper}>
+        <Price product={product} button={false} quantity={quantity} />
+        <RemoveFromCartButton product={product} />
+      </div>
  
     </li>
   )
