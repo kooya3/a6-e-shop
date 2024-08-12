2d6c64ee70f327bd547b69af8d7a275ecec6211b
Elyees Tatua <elyeestatua@gmail.com>
2024-08-12 19:31:25 +0300

    refactor(CheckoutPage): Remove unused code and include lazy loading for images

--- src/app/(pages)/checkout/page.tsx
+++ src/app/(pages)/checkout/page.tsx
@@ -29,10 +29,6 @@ export default async function Checkout() {
   }
 
   return (
-                                <Fragment>
-                                  
-
-                                </Fragment>
   )
 }
 
