584897183949084ad61f6c08e9b2b5cac1f5ea42
Elyees Tatua <elyeestatua@gmail.com>
2024-08-12 03:24:40 +0300

    refactor(CartItem): Update CartItem component to include lazy loading for images

--- src/app/(pages)/cart/CartItem/index.tsx
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -1,6 +1,13 @@
 'use client'
 
 import React, { useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Media } from '../../../_components/Media'
+import { Price } from '../../../_components/Price'
+import { RemoveFromCartButton } from '../../../_components/RemoveFromCartButton'
+
 
   )
 }
