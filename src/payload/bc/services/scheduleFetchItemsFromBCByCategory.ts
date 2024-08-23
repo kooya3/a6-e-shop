ecc3224e4ec7c26b69284ee1d93ba16c47d91a8e
Suleiman Yunus <kayangasuleiman@gmail.com>
2024-08-23 16:30:36 +0300

    feat(products): add fetch by dynamic categories

--- src/payload/bc/services/scheduleFetchItemsFromBCByCategory.ts
+++ src/payload/bc/services/scheduleFetchItemsFromBCByCategory.ts
@@ -1,15 +1,33 @@
 import payload from 'payload'
 
 import { fetchItemsFromBCByCategory } from '../endpoints/fetchItemsFromBCByCategory'
-import { createImage } from '../utilities/create-image';
-import { createProduct } from '../utilities/create-product';
+import { createImage } from '../utilities/create-image'
+import { createProduct } from '../utilities/create-product'
+
+export const fetchCategoryCodes = async (): Promise<string[]> => {
+  try {
+    const categories = await payload.find({
+      collection: 'categories',
+    })
+
+    return categories.docs.map(category => category.code as string)
+  } catch (error: unknown) {
+    payload.logger.error('Error fetching category codes')
+    throw new Error('Error fetching category codes')
+  }
+}
 
 export const scheduleFetchProductsFromBusinessCentral = async (): Promise<void> => {
   try {
-    const items = await fetchItemsFromBCByCategory({ categories: ['CAT04', 'CAT08', 'CAT10', 'CAT11', 'CAT12', 'CAT13', 'CAT15', 'CAT16', 'CAT18'] });
+    const categoryCodes = await fetchCategoryCodes()
+
+    const items = await fetchItemsFromBCByCategory({
+      categories: categoryCodes,
+    })
+
     const noImageAvailableID = await createImage()
 
-    for (const item of items.value) {
+    for (const item of items) {
       const existingProduct = await payload.find({
         collection: 'products',
         where: {
@@ -17,7 +35,7 @@ export const scheduleFetchProductsFromBusinessCentral = async (): Promise<void> 
             equals: item.No,
           },
         },
-      });
+      })
 
       if (existingProduct.docs.length > 0) {
         await payload.update({
@@ -29,22 +47,32 @@ export const scheduleFetchProductsFromBusinessCentral = async (): Promise<void> 
             unitPrice: item.Unit_Price,
             inventory: item.Inventory,
           },
-        });
-      } else {
+        })
+      } else if (item.Description && item.No && item.Gen_Prod_Posting_Group && item.Inventory && item.Unit_Price) {
         const product = createProduct(item)
+        const category = await payload.find({
+          collection: 'categories',
+          where: {
+            code: {
+              equals: item.Gen_Prod_Posting_Group,
+            },
+          },
+        })
 
-        await payload.create({
-          collection: 'products',
-          data: JSON.parse(
-            JSON.stringify({ ...product }).replace(
-              /"\{\{PRODUCT_IMAGE\}\}"/g,
-              noImageAvailableID.toString(),
+        if (category.docs.length > 0) {
+          await payload.create({
+            collection: 'products',
+            data: JSON.parse(
+              JSON.stringify({ ...product, categories: [category.docs[0].id] }).replace(
+                /"\{\{PRODUCT_IMAGE\}\}"/g,
+                noImageAvailableID.toString(),
+              ),
             ),
-          ),
-        });
+          })
+        }
       }
     }
   } catch (error: unknown) {
-    payload.logger.info('Error fetching products:', error);
+    payload.logger.info('Error fetching products:', error)
   }
 }
