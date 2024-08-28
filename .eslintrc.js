06ce5f51aea5eac909ca920b88e21c57f6f08091
Elyees Tatua <elyeestatua@gmail.com>
2024-08-28 13:29:07 +0300

    fix(eslint): add eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
-  root: true,
-  extends: ['plugin:@next/next/recommended', '@payloadcms'],
-  ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
-    'no-console': 'off',
-  },
-}
+    root: true,
+    extends: ['plugin:@next/next/recommended', '@payloadcms'],
+    ignorePatterns: ['**/payload-types.ts'],
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': 'off', // Disable Prettier rule
+      'no-console': 'off', // Disable no-console rule
+      'no-unused-vars': 'off',
+    }
+  };
